b2e8bd6a5be626f8359775509167308f37417236
root <root@localhost.(none)>
2019-09-28 22:11:27 +0300

    not a major addition

--- mcastcmds-nt2.c
+++ mcastcmds-nt2.c
@@ -95,8 +95,8 @@ char *buffer = (char *) malloc(sizeof(char *)*size);
 int numr; 
 sc=sendto(so,filename,strlen(filename)+1, 0, (struct sockaddr *) &mcast, sizeof(mcast)); 
 if(sc==-1) printf("Unable to send, do group exist\n");
-int ntimes2=ntimes*2;
-for(i=0; i<ntimes2; i++)
+int ntimes1=ntimes*2;
+for(i=0; i<ntimes1; i++)
 while((numr=fread(buffer,sizeof(char),size,fp))!=0);
 
 for(i=0;i< ntimes;i++){
